0dddf8d961ff7efb569afb14fb549d085346516e
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-12-24 21:46:26 +0900

    [add] 最初のプッシュ。OpenUPMでUniRxを導入してみた。

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
